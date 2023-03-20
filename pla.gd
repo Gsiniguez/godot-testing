18347e99144908f7b9db2de9210bbe3c561523ee
Gonzalo Iniguez <gonza.ini94@gmail.com>
2023-03-20 15:07:53 -0300

    rotate in progress

--- pla.gd
+++ pla.gd
@@ -1,18 +1,14 @@
 extends CharacterBody3D
 
+
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
-
-@onready var pivot = $CollisionShape3D/Pivot
-@onready var mark = $CollisionShape3D/Mark
-
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
+
 func _physics_process(delta):
-	if not is_multiplayer_authority(): return
-	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
@@ -23,7 +19,7 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("left", "right", "forward", "back")
+	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
@@ -31,23 +27,5 @@ func _physics_process(delta):
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-	
-	#LOOK AT MOVEMENT
-	var future_position = Vector3(pivot.position.x + input_dir.x , 1, pivot.position.z + input_dir.y)
-	mark.position = future_position
 
-	pivot.look_at(transform.origin + future_position, Vector3.UP)
-	#pivot.look_at(mark.position, Vector3(0,1,0))
-	
 	move_and_slide()
-	rpc("handle_position", position)
-
-@rpc("reliable")
-func handle_position(authority_position):
-	print("SET GLOBAL POSITION")
-	self.position = authority_position
-
-@rpc("reliable")
-func handle_rotation(authority_rotation):
-	print("SET GLOBAL POSITION")
-	self.rotation = authority_rotation
