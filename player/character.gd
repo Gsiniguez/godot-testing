4f73ea79f938965c3f9ef0aa9f847fedd3bcaeb9
Gonzalo Iñiguez <31194342+Gsiniguez@users.noreply.github.com>
2023-03-21 21:43:19 -0300

    speed acceleration adn friction

--- player/character.gd
+++ player/character.gd
@@ -1,6 +1,8 @@
 extends CharacterBody3D
 
 const SPEED = 5.0
+const ACCELERATION = 0.3
+const FRICTION = 0.1
 const JUMP_VELOCITY = 4.5
 
 
@@ -22,10 +24,13 @@ func _ready():
 func _physics_process(delta):
 	if not _sync.is_multiplayer_authority(): return
 	
+	
 	# Add the gravity.
 	if not is_on_floor():
 		self.velocity.y -= gravity * delta
-
+		_animation_tree.set("parameters/a-g/transition_request","on_air")
+		_animation_tree.set("parameters/jump_blend/blend_position", clamp(velocity.y, -1,1))
+	
 	# Handle Jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		self.velocity.y = JUMP_VELOCITY
@@ -35,10 +40,12 @@ func _physics_process(delta):
 	move_direction.x = Input.get_action_strength("right") - Input.get_action_strength("left")
 	move_direction.z = Input.get_action_strength("back") - Input.get_action_strength("forward")
 	move_direction = move_direction.rotated(Vector3.UP, _spring_arm.rotation.y).normalized()
-	velocity.x = move_direction.x * SPEED
-	velocity.z = move_direction.z * SPEED
-	print(velocity.normalized())
-	_animation_tree.set("parameters/i-w-r/blend_amount",0.3)
+	if is_on_floor():
+		velocity = velocity.move_toward(SPEED * move_direction , ACCELERATION)
+		velocity = velocity.move_toward(Vector3.ZERO, FRICTION)
+		_animation_tree.set("parameters/a-g/transition_request","on_ground")
+		_animation_tree.set("parameters/i-w-r/blend_amount",clamp(velocity.length(), 0 ,1))
+	
 	# LOOK AT
 	if move_direction:
 		_pivot.look_at(global_position - move_direction * 100, Vector3.UP)
